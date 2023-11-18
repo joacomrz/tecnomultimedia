aab883a0a4d207b4cfe02d7643a516ef49b711c8
joacomrz <130106706+joacomrz@users.noreply.github.com>
2023-11-18 17:16:41 -0300

    Delete tp5/tp5 directory

--- tp5/tp5/jugador.js
+++
@@ -1,9 +0,0 @@
-class Jugador {
-  constructor() {
-    this.x = width / 2;
-  }
-
-  mostrar() {
-    image(juego.imagenJugador, this.x - 20, height - 40, 40, 40);
-  }
-}
--- tp5/tp5/tablero.js
+++
@@ -1,7 +0,0 @@
-class Tablero {
-  mostrar() {
-    fill(255, 0, 0);
-    textSize(50);
-    text("Puntaje: " + juego.puntaje, 40, 70);
-  }
-}
