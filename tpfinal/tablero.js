d88a2d7014671a18032196a0fe9dbe3ac879014f
joacomrz <130106706+joacomrz@users.noreply.github.com>
2023-12-09 16:50:28 -0300

    Delete tpfinal directory

--- tpfinal/tablero.js
+++
@@ -1,8 +0,0 @@
-class Tablero {
-  mostrar() {
-    fill(255, 0, 0);
-    textSize(50);
-    let anchuraTexto = textWidth("Puntaje: " + juego.puntaje);
-    text("Puntaje: " + juego.puntaje, width / 1.8 - anchuraTexto / 2, 70);
-  }
-}
